e610eb166f5d23d55f88255cdf8b21dc4387ae44
soroushCoder <soroush.e89@gmail.com>
2018-05-17 21:47:09 +0430

    add recommended product

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Theme Soroush',
+    'name': 'Theme Soroush first',
     'description': 'website theme to showcase customization possibilities.',
     'category': 'Theme/Shoping',
     'sequence': 1000,
@@ -13,7 +13,8 @@
         'data/theme_default_data.xml',
         'views/theme_default_templates.xml',  
         'views/ecommerce_snippest_extra.xml',
-        'views/website_ecommerce_template_views.xml'
+        'views/website_ecommerce_template_views.xml',
+        'views/template.xml'
     ],
     'images': [
         'static/description/cover.png',
